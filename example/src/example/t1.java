22f13f609c1d8e5c266882c0d88c6e11b9ef73d4
Shah Manish <manishshah1698@gmail.com>
2020-09-19 22:45:27 +0530

    Test for divisible by 5

--- example/src/example/t1.java
+++ example/src/example/t1.java
@@ -7,10 +7,19 @@ import org.junit.jupiter.api.Test;
 class t1 {
 
 	@Test
+	// Divisible by 3
 	public void testForDivisibleBy3() {
 		exm junit = new exm(); 
 		String result = junit.fizzBuzz(3);
 		assertEquals("Fizz",result);
+	}
+	
+	@Test
+	// Divisible by 5
+	public void testForDivisibleBy5() {
+		exm junit = new exm(); 
+		String result = junit.fizzBuzz(5);
+		assertEquals("Buzz",result);
 	}
 
 	
